fff8365512504e3820fcf674ba7b5e1e7a8adc10
LYJ <lyj.blessed2@gmail.com>
2024-12-19 11:08:09 +0900

    feat: exception 1차 commit

---
+++ src/main/java/com/sparta/outsourcing/exception/WrongAccessException.java
@@ -0,0 +1,10 @@
+package com.sparta.outsourcing.exception;
+
+import lombok.Getter;
+
+@Getter
+public class WrongAccessException extends OrderException {
+//    public WrongAccessException (final Errorcode errorcode) {
+//        super(errorcode);
+//    }
+}
