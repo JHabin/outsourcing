1e2b7a9b40619e231b10d665ecbe755fed9a7797
LYJ <lyj.blessed2@gmail.com>
2024-12-20 09:43:18 +0900

    feat: CRUD 2차 commit

--- src/main/java/com/sparta/outsourcing/service/OrderService.java
+++ src/main/java/com/sparta/outsourcing/service/OrderService.java
@@ -1,12 +1,20 @@
 package com.sparta.outsourcing.service;
 
+import com.sparta.outsourcing.common.Authentication;
+import com.sparta.outsourcing.common.Role;
 import com.sparta.outsourcing.common.Status;
+import com.sparta.outsourcing.constants.SessionNames;
 import com.sparta.outsourcing.dto.order.*;
 import com.sparta.outsourcing.entity.Menu;
 import com.sparta.outsourcing.entity.Order;
+import com.sparta.outsourcing.entity.User;
 import com.sparta.outsourcing.exception.BadValueException;
+import com.sparta.outsourcing.exception.ErrorCode;
 import com.sparta.outsourcing.exception.WrongAccessException;
 import com.sparta.outsourcing.repository.OrderRepository;
+import com.sparta.outsourcing.repository.UserRepository;
+import com.sparta.outsourcing.repository.menu.MenuRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,50 +30,52 @@ import java.util.List;
 @RequiredArgsConstructor //의존성 주입
 public class OrderService {
     private final OrderRepository orderRepository;
-    //private final UserRepository userRepository;
-    //private final MenuRepository menuRepository;
+    private final UserRepository userRepository;
+    private final MenuRepository menuRepository;
 
     /**
      * 주문 생성
      * request(필수) : menuId, count
      */
     @Transactional
-    public CreateOrderResponseDto createOrder(CreateOrderRequestDto createOrderRequestDto) {
-//        Menu menu = menuRepository.findByIdOrElseThrow(createOrderRequestDto.getMenuId());
-//
-//        //영업시간이 아닐 시 예외처리
-//        LocalTime nowLocalTime = LocalTime.now(); //현재시간
-//        LocalTime storeOpenTime = menu.getStore().getOpenTime(); //가게 오픈시간
-//        LocalTime storeCloseTime = menu.getStore().getCloseTime(); //가게 마감시간
-//
-//        if (nowLocalTime.isBefore(storeOpenTime) || nowLocalTime.isAfter(storeCloseTime)) {
-//            //throw new WrongAccessException(ErrorCode.NOT_OPEN_STORE); //ErrorCode에 NOT_OPEN_STORE 추가 필요**
-//        }
-//
-//        //주문금액 계산 및 최소주문금액 충족하지 못할 시 예외처리
-//        Integer totalPrice = createOrderRequestDto.getCount() * menu.getPrice(); //총 주문금액
-//        Integer minOrderPrice = menu.getStore().getMinOrderPrice(); //가게의 최소주문금액
-//
-//        if (totalPrice < minOrderPrice) {
-//            throw new BadValueException(ErrorCode.LESS_THAN_MINPRICE); //ErrorCode 추가 필요**
-//        }
-//
-//        //이상 없을 시, 주문 등록 처리
-//        User user= userService.findUserById(createOrderRequestDto.getUserId()); //email로도 유저 조회 가능
-//        Order order = new Order(user, menu, createOrderRequestDto.getCount(), totalPrice, Status.ORDER_COMPLETE);
-//        Order savedOrder = orderRepository.save(order);
+    public CreateOrderResponseDto createOrder(CreateOrderRequestDto createOrderRequestDto,
+                                              HttpServletRequest httpServletRequest) {
+        Menu menu = menuRepository.findByIdOrElseThrow(createOrderRequestDto.getMenuId());
 
-//        return new CreateOrderResponseDto(
-//                savedOrder.getUser().getId(),
-//                savedOrder.getMenu().getId(),
-//                savedOrder.getCount(),
-//                savedOrder.getTotalPrice(),
-//                savedOrder.getStatus(),
-//                savedOrder.getRejectedReason(),
-//                savedOrder.getCreatedAt(),
-//                savedOrder.getModifiedAt()
-//        );
-        return null;
+        //영업시간이 아닐 시 예외처리
+        LocalTime nowLocalTime = LocalTime.now(); //현재시간
+        LocalTime storeOpenTime = menu.getStore().getOpenTime(); //가게 오픈시간
+        LocalTime storeCloseTime = menu.getStore().getCloseTime(); //가게 마감시간
+
+        if (nowLocalTime.isBefore(storeOpenTime) || nowLocalTime.isAfter(storeCloseTime)) {
+            throw new WrongAccessException(ErrorCode.NOT_OPEN_STORE);
+        }
+
+        //주문금액 계산 및 최소주문금액 충족하지 못할 시 예외처리
+        Integer totalPrice = createOrderRequestDto.getCount() * menu.getPrice(); //총 주문금액
+        Integer minOrderPrice = menu.getStore().getMinOrderPrice(); //가게의 최소주문금액
+
+        if (totalPrice < minOrderPrice) {
+            throw new BadValueException(ErrorCode.LESS_THAN_MINPRICE);
+        }
+
+        //이상 없을 시, 주문 등록 처리
+        //유저 정보 조회
+        Authentication auth = (Authentication) httpServletRequest.getSession(false).getAttribute(SessionNames.USER_AUTH);
+        User user = userRepository.findByEmailOrElseThrow(auth.getEmail());
+        Order order = new Order(user, menu, createOrderRequestDto.getCount(), totalPrice, Status.ORDER_COMPLETE);
+        Order savedOrder = orderRepository.save(order);
+
+        return new CreateOrderResponseDto(
+                savedOrder.getUser().getId(),
+                savedOrder.getMenu().getId(),
+                savedOrder.getCount(),
+                savedOrder.getTotalPrice(),
+                savedOrder.getStatus(),
+                savedOrder.getRejectedReason(),
+                savedOrder.getCreatedAt(),
+                savedOrder.getModifiedAt()
+        );
     }
 
     /**
@@ -98,8 +108,10 @@ public class OrderService {
         Order order = orderRepository.findByIdOrElseThrow(id);
         //2. 주문 수정 (주문 거절)
         //   2-1. 거절 사유 추가
-        //order.update(rejectReason);
-        //   2-2. 거절 시간 추가 (soft delete)
+        order.reject(rejectReason);
+        //   2-2. 거절 시간 추가 (softDelete: deleted 시간 추가)
+        order.softDelete();
+
         return new RejectOrderResponseDto(
                 order.getUser().getId(),
                 order.getMenu().getId(),
@@ -131,14 +143,14 @@ public class OrderService {
         );
     }
 
-    /**
-     * 주문 다건 조회
-     * @RequestParam
-     */
-    public List<OrderResponseDto> findAll() {
-        //return orderRepository.findAllByStore(store).stream().map(OrderResponseDto::toDto).toList();
-        return null;
-    }
+//    /**
+//     * 주문 다건 조회
+//     * @RequestParam
+//     */
+//    public List<OrderResponseDto> findAll() {
+//        //return orderRepository.findAllByStore(store).stream().map(OrderResponseDto::toDto).toList();
+//        return null;
+//    }
 
 
 }
