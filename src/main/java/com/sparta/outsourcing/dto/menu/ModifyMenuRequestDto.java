aa62b06af45a93bc91da94b0ba0141878189fd5b
JeongMinAhnn <133354826+JeongMinAhnn@users.noreply.github.com>
2024-12-20 16:15:43 +0900

    feat : 필드가 하나인 RequestDto 경우 생성자의 JsonCreator 어노테이션을 추가

--- src/main/java/com/sparta/outsourcing/dto/menu/ModifyMenuRequestDto.java
+++ src/main/java/com/sparta/outsourcing/dto/menu/ModifyMenuRequestDto.java
@@ -1,5 +1,6 @@
 package com.sparta.outsourcing.dto.menu;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
@@ -7,7 +8,6 @@ import lombok.Getter;
  * 클래스의 필드에 대해 Getter 메서드를 자동으로 생성
  */
 @Getter
-
 /*
  * 메뉴 수정 요청을 처리하기 위한 Data Transfer Object(DTO) 클래스
  * 클라이언트로부터 전달받은 메뉴 이름 데이터를 검증하고 저장
@@ -26,8 +26,11 @@ public class ModifyMenuRequestDto {
    * ModifyMenuRequestDto 객체를 생성
    *
    * @param name 수정할 메뉴의 이름
+   *             필드가 하나인 RequestDto 경우 생성자의 JsonCreator 어노테이션을 추가해야한다.
    */
+  @JsonCreator
   public ModifyMenuRequestDto(String name) {
     this.name = name;
   }
+
 }
