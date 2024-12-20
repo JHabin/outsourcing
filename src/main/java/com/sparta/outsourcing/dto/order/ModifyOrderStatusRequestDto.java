bf8d31d9e4f317796c5aa55c4d7e8d1fa3e7c502
LYJ <lyj.blessed2@gmail.com>
2024-12-20 18:20:04 +0900

    feat: RequestDto에서 사용하지 않는 필드 삭제, JsonCreator 어노테이션 추가

--- src/main/java/com/sparta/outsourcing/dto/order/ModifyOrderStatusRequestDto.java
+++ src/main/java/com/sparta/outsourcing/dto/order/ModifyOrderStatusRequestDto.java
@@ -1,19 +1,17 @@
 package com.sparta.outsourcing.dto.order;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class ModifyOrderStatusRequestDto {
-    private final Long menuId;
-    private final Integer count;
-
     @NotBlank(message = "변경할 상태값은 필수 입력사항입니다.")
     private final String status;
 
-    public ModifyOrderStatusRequestDto(Long menuId, Integer count, String status, String rejectReason) {
-        this.menuId = menuId;
-        this.count = count;
+    //필드가 하나일 때 @RequestBody 정상적으로 동작할 수 있도록 하는 어노테이션
+    @JsonCreator
+    public ModifyOrderStatusRequestDto(String status) {
         this.status = status;
     }
 }
--- src/main/java/com/sparta/outsourcing/dto/order/RejectOrderRequestDto.java
+++ src/main/java/com/sparta/outsourcing/dto/order/RejectOrderRequestDto.java
@@ -1,21 +1,16 @@
 package com.sparta.outsourcing.dto.order;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class RejectOrderRequestDto {
-    private final Long menuId;
-    private final Integer count;
-    private final String status;
-
     @NotBlank(message = "거절사유는 필수 입력 사항입니다.")
     private final String rejectReason;
 
-    public RejectOrderRequestDto(Long menuId, Integer count, String status, String rejectReason) {
-        this.menuId = menuId;
-        this.count = count;
-        this.status = status;
+    @JsonCreator
+    public RejectOrderRequestDto(String rejectReason) {
         this.rejectReason = rejectReason;
     }
 }
